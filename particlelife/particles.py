68087ea9131cee105bcf11dd54f5f92b45403af0
Laura Grozdanic <Laura.grozdanic@study.hs-duesseldorf.de>
2025-12-10 15:23:37 +0100

    added class particles

--- particlelife/particles.py
+++ particlelife/particles.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-def diffuse(x, y, n_step=0.1):
-    x += np.random.normal(scale=n_step, size=x.shape)
-    y += np.random.normal(scale=n_step, size=y.shape)
-    return x, y
+class Particles:
+    def __init__ (self, n_points=1000):
+        self.n_points = n_points
+        self.x = np.random.normal(loc=0.0, scale=10.0, size=n_points)
+        self.y = np.random.normal(loc=0.0, scale=10.0, size=n_points)
+
+
+    def diffuse(self, x, y, n_step=0.1):
+        self.x += np.random.normal(scale=n_step, size=x.shape)
+        self.y += np.random.normal(scale=n_step, size=y.shape)
+        return x, y
