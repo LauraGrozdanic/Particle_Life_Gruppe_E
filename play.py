ac5966ef3dcbe6f64c4d6aba750f7bd4bfd329f7
Sahar <saharr.malakan@study.hs-duesseldorf.de>
2025-12-10 15:23:01 +0100

    added function to run visualization

--- play.py
+++ play.py
@@ -1,1 +1,11 @@
+from particlelife.visualization import Visualization
+from vispy import app
 
+
+if __name__ == "__main__":
+    #visualization = Visualization()
+    #visualization.run()
+    app.create()
+    win = Visualization()
+    win.show()
+    app.run()
