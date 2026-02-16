ab08b2a89edb0f21b45e2c472a380fc0b4e9c05c
Laura Grozdanic <Laura.grozdanic@study.hs-duesseldorf.de>
2026-02-16 22:59:30 +0100

    add unit test fpr diffuse()

--- test/test_particles.py
+++ test/test_particles.py
@@ -19,3 +19,25 @@ def test_wrap_around():
     # testen ob alle wieder im Bereich sind
     assert np.all(p.x >= xmin) and np.all(p.x < xmax)
     assert np.all(p.y >= ymin) and np.all(p.y < ymax)
+
+def test_diffuse():
+    # Erstellt 2 Partikel
+    p = Particles(n_points=2)
+
+    #feste Startposition (nichts ist random)
+    p.x = np.array([0.0, 1.0])
+    p.y = np.array([10.0, 20.0])
+
+    # feste Geschwindigkeit
+    p.vx = np.array([1.0, -2.0])
+    p.vy = np.array([0.5, 0.0])
+
+    # kein Zufallsrauschen, nur Bewegung durch vx/vy
+    x_new, y_new = p.diffuse(n_step=0.0)
+
+    expected_x = np.array([1.0, -1.0])
+    expected_y = np.array([10.5, 20.0])
+
+    # prüft ob es genau stimmt 
+    assert (x_new == expected_x).all()
+    assert (y_new == expected_y).all()
