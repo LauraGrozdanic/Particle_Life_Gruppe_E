688cec1ac87b13a005dca6ccfaf439e4bab2591a
Laura Grozdanic <Laura.grozdanic@study.hs-duesseldorf.de>
2026-02-16 11:28:02 +0100

    clean up interaction.py and add docstrings

--- particlelife/interaction.py
+++ particlelife/interaction.py
@@ -1,20 +1,29 @@
-# Interaktion zwischen partikeln
+"""
+interaction.py
+
+Computes interaction forces between particles based on their types.
+Uses Numba (@njit) to run faster.
+
+"""
 
 import numpy as np
 from numba import njit
 
+# types: 0=Turq, 1=Red, 2=Green, 3=Yellow
 INTERACTION_MATRIX = np.array([
-    #        0 (Türkis)  1 (Rot)   2 (Grün)  3 (Gelb)
-        [-0.3,  0.2, -0.1,  0.4],
-        [ 0.1, -0.3,  0.3, -0.2],
-        [-0.4,  0.1, -0.3,  0.2],
-        [ 0.3, -0.4,  0.2, -0.3],
+        [-0.3,  0.2, -0.1,  0.4], # Turquoise reacts to (Turq, Red, Green, Yellow)
+        [ 0.1, -0.3,  0.3, -0.2], # Red reacts to (Turq, Red, Green, Yellow)
+        [-0.4,  0.1, -0.3,  0.2], # Green reacts to (Turq, Red, Green, Yellow)
+        [ 0.3, -0.4,  0.2, -0.3], # Yellow reacts to (Turq, Red, Green, Yellow)
 ])
 
 @njit
 def compute_forces(x, y, types, matrix, max_distance, interaction_strength):
+    """ 
+    Calculate the force (fx, fy) on each particle from all other particles.
+    Only particles closer than max_distance interact.
+    """
     n = len(x)
-
     fx = np.zeros(n)
     fy = np.zeros(n)
 
@@ -35,7 +44,6 @@ def compute_forces(x, y, types, matrix, max_distance, interaction_strength):
             direction_y = dy / distance
 
             strength = matrix[types[i], types[j]] * interaction_strength
-
             r = distance / max_distance
 
             if r < 0.15:
@@ -51,36 +59,6 @@ def compute_forces(x, y, types, matrix, max_distance, interaction_strength):
             fy[i] += direction_y * force
 
     return fx, fy
-#def compute_interaction_direction(pos_i, pos_j, type_i, type_j,
-#                                  matrix=INTERACTION_MATRIX,
- #                                 max_distance=50,
-  #                                interaction_strength=1.0):
-#
- #   delta = pos_j - pos_i
-  #  distance = np.linalg.norm(delta)
-#
- #   if distance == 0 or distance > max_distance:
-  #      return np.zeros(2)
-#
- #   # Richtung normieren
-  #  direction = delta / distance
-#
- #   # Stärke aus Matrix
-  #  strength = matrix[type_i][type_j] * interaction_strength
-
-
-   # r = distance / max_distance
-
-   # if r < 0.1:
-   #     factor = -2.0 * (0.1 - r)        # starke Repulsion
-    #elif r < 0.4:
-     #   factor = 1.5 * (r - 0.1)         # Attraction
-  #  elif r < 0.7:
-   #     factor = -0.5 * (r - 0.4)        # leichte Repulsion
-    #else:
-     #   factor = 0.0
-     
-    #return direction * strength * factor
 
 
 
