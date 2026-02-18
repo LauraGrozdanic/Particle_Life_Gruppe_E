f5a7bed1b3913fee0ee55e349ef5d9400ae7c0f8
Linnea Brehe <linnea.brehe@study.hs-duesseldorf.de>
2026-02-18 11:36:21 +0100

    made stylistic changes

--- test/test_interaction.py
+++ test/test_interaction.py
@@ -1,6 +1,7 @@
 import numpy as np
 from particlelife.interaction import compute_forces, INTERACTION_MATRIX
 
+
 def test_forces_no_nan_():
     x = np.array([0.0, 0.0, 1.0])
     y = np.array([0.0, 0.0, 0.0])
@@ -9,29 +10,21 @@ def test_forces_no_nan_():
     interaction_strength = 1.0
 
     fx, fy = compute_forces(
-        x,
-        y,
-        types,
-        INTERACTION_MATRIX,
-        max_distance,interaction_strength
+        x, y, types, INTERACTION_MATRIX, max_distance, interaction_strength
     )
     assert np.isfinite(fx).all()
     assert np.isfinite(fy).all()
-#=================================second test=================================
+
+
 def test_nointeraction_when_far():
-    #Particles farther apart than max_distance should not interact.The resulting forces must be zero.
+    # Particles farther apart than max_distance should not interact.The resulting forces must be zero.
     x = np.array([0.0, 100.0])
     y = np.array([0.0, 0.0])
     types = np.array([0, 1], dtype=np.int64)
     max_distance = 10.0
     interaction_strength = 1.0
     fx, fy = compute_forces(
-        x,
-        y,
-        types,
-        INTERACTION_MATRIX,
-        max_distance,
-        interaction_strength
+        x, y, types, INTERACTION_MATRIX, max_distance, interaction_strength
     )
 
     assert np.allclose(fx, 0.0)
